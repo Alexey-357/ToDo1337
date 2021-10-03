4e25be8d942ae087bf7469da7397a982453b698f
Mishchenko <mishchenko.o@web.de>
2021-10-03 16:40:50 +0200

    some js

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-let form = document.getElementById('listForm');
+/*let form = document.getElementById('listForm');
 form.addEventListener('submit', (event) => {
    form.submit(); // handle the form data
-});
+});*/
+
+
+function paintLabels(){
+  
+
+   let lf=document.getElementById('listForm');
+let items=lf.getElementsByTagName("input");
+for(let item of items){
+   if(item.checked == true){
+      item.parentElement.style.background = "green";
+   }
+}
+}
